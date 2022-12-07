54ef4af655b186315267d3b5c779a49f9b8fad64
Sunthewhat <81130744+Sunthewhat@users.noreply.github.com>
2022-12-07 22:47:07 +0700

    jjj

--- CSC102/Final_Prep/CC.java
+++ CSC102/Final_Prep/CC.java
@@ -21,7 +21,7 @@ public class CC {
     public static void main(String[] args) {
         Complex c1 = new Complex(10, 15);
         Complex c2 = new Complex(c1);
-        Complex c3 = c1;
+        // Complex c3 = c1;
         System.out.println(c2);
     }
 }
--- CSC102/Final_Prep/Final_Practice_04.java
+++ CSC102/Final_Prep/Final_Practice_04.java
@@ -1,6 +1,6 @@
 public class Final_Practice_04 {
 
-    private int multiply(int x, int y) {
+    int multiply(int x, int y) {
         return x * y;
     }
 }
