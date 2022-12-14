b6e3075057296a3daaaeb8c5b58c600f496f07fa
Sunthewhat <81130744+Sunthewhat@users.noreply.github.com>
2022-12-14 15:10:23 +0700

    asdasd

---
+++ CSCMSME/Contest_DoOrDie.java/FruitPicker.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class FruitPicker {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int d = sc.nextInt();
+        n *= m;
+        int sum = 0;
+        int[] basket = new int[n];
+        for (int i = 0; i < n; i++)
+            basket[i] = sc.nextInt();
+        Arrays.sort(basket);
+        int[] re_basket = new int[n];
+        int len = basket.length - 1;
+        for (int i = 0; i < basket.length; i++) {
+            re_basket[i] = basket[len--];
+        }
+        for (int i = 0; i < d; i++) {
+            sum += re_basket[i] - i;
+        }
+        System.out.println(sum);
+        sc.close();
+    }
+}
