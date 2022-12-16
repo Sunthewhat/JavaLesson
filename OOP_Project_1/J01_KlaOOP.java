ae089a465af1a0d40aee62b214da965e08cf143e
Sunthewhat <81130744+Sunthewhat@users.noreply.github.com>
2022-12-16 21:20:20 +0700

    sdsdf

---
+++ OOP_Project_1/J01_KlaOOP.java
@@ -0,0 +1,18 @@
+public class J01_KlaOOP {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Dog {
+    // Attributes
+    private int age;
+    private double helth;
+    private double heigth;
+    private String name;
+    private String owner;
+    public static int dogCount = 0;
+    // Constructors
+
+    // Methods
+}
