44e0efb1eb28d740afd6a5bd9b226b61278dd1a1
Sunthewhat <81130744+Sunthewhat@users.noreply.github.com>
2022-12-19 16:31:40 +0700

    sdf

--- CSC102/DoOrDie2022/Q1Efficient.java
+++ CSC102/DoOrDie2022/Q1Efficient.java
@@ -1,4 +1,4 @@
-public class Q1Mew {
+public class Q1Efficient {
     public static void main(String[] args) {
         java.util.Scanner sc = new java.util.Scanner(System.in);
         String s = sc.nextLine();
