477f91dfa0e0ff11e15ab6856e6ad90c908cf375
Sunthewhat <81130744+Sunthewhat@users.noreply.github.com>
2022-12-21 15:55:32 +0700

    sdf

---
+++ CSC102/Mark_reDnD_Prep/arrays_1.java
@@ -0,0 +1,10 @@
+public class arrays_1 {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int field[][] = new int[n][m];
+        
+        sc.close();
+    }
+}
--- CSCMSME/CPP158_Max_contiguous_sum.cpp
+++ CSCMSME/CPP158_Max_contiguous_sum.cpp
@@ -1,7 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+int dp[1100];
+
 int main() {
-  int len, range;
-  cin >> len >> range;
-  return 0;
+     int n, range;
+     cin >> n >> range;
+     dp[0] = 0;
+     int a[n];
+     for (int i = 1; i <= n; i++) {
+          int tmp;
+          cin >> tmp;
+          dp[i] = dp[i - 1] + tmp;
+     }
+     int mx = -10e9;
+     for (int i = range; i <= n; i++) {
+          mx = max(mx, dp[i] - dp[i - range]);
+     }
+     cout << mx;
+     return 0;
 }
