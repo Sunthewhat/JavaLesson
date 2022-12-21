1c5b584411b1e044926a4e2356b6ad4bdb41b4b8
Sunthewhat <81130744+Sunthewhat@users.noreply.github.com>
2022-12-21 14:19:30 +0700

    asda

--- CSCMSME/CPP019_The_power_of_power_CPP.cpp
+++ CSCMSME/CPP019_The_power_of_power_CPP.cpp
@@ -1,29 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-long long rec(long long a, long long b, long long c)
-{
-    if (b == 0ll)
-        return 1ll % c;
-    if (b == 1ll)
-        return a % c;
-    long h = rec(a, b / 2, c);
-    long f = (h * h) % c;
-    if (b % 2 == 1ll)
-        f = (f * a) % c;
-    return f;
+long long rec(long long a, long long b, long long c) {
+  if (b == 0ll)
+    return 1ll % c;
+  if (b == 1ll)
+    return a % c;
+  long h = rec(a, b / 2, c);
+  long f = (h * h) % c;
+  if (b % 2 == 1ll)
+    f = (f * a) % c;
+  return f;
 }
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int q;
-    cin >> q;
-    while (q--)
-    {
-        long long a, b, c;
-        cin >> a >> b >> c;
-        cout << rec(a, b, c) << endl;
-    }
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int q;
+  cin >> q;
+  while (q--) {
+    long long a, b, c;
+    cin >> a >> b >> c;
+    cout << rec(a, b, c) << endl;
+  }
 
-    return 0;
+  return 0;
 }
--- CSCMSME/CPP_153_Wet.cpp
+++ CSCMSME/CPP_153_Wet.cpp
@@ -1,29 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 int ans[10000000];
-int main()
-{
-    int n, q;
-    cin >> n >> q;
-    double x[n], y[n], r[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> x[i] >> y[i] >> r[i];
-    }
-    for (int i = 0; i < q; i++)
-    {
-        double xp, yp;
-        cin >> xp >> yp;
-        for (int j = 0; j < n; j++)
-        {
-            double dis = sqrt(pow(x[j] - xp, 2) + pow(y[j] - yp, 2));
-            if (r[j] >= dis)
-                ans[i]++;
-        }
-    }
-    for (int i = 0; i < q; i++)
-    {
-        cout << ans[i] << endl;
-    }
-    return 0;
+int main() {
+   int n, q;
+   cin >> n >> q;
+   double x[n], y[n], r[n];
+   for (int i = 0; i < n; i++) {
+      cin >> x[i] >> y[i] >> r[i];
+   }
+   for (int i = 0; i < q; i++) {
+      double xp, yp;
+      cin >> xp >> yp;
+      for (int j = 0; j < n; j++) {
+         double dis = sqrt(pow(x[j] - xp, 2) + pow(y[j] - yp, 2));
+         if (r[j] >= dis) ans[i]++;
+      }
+   }
+   for (int i = 0; i < q; i++) {
+      cout << ans[i] << endl;
+   }
+   return 0;
 }
