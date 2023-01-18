f9e04a78c0d8cb9932066a5a3619557128cb8022
Sunthewhat <81130744+Sunthewhat@users.noreply.github.com>
2023-01-18 14:56:18 +0700

    j j

---
+++ CSCMSME/CPP155_Racing.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+     // ios::sync_with_stdio(0);
+     // cin.tie(0);
+     int n;
+     cin >> n;
+     int dis;
+     cin >> dis;
+     double time[n], timesort[n];
+     for (int i = 0; i < n; i++) {
+          int diffdis, v;
+          double tmptime;
+          cin >> diffdis >> v;
+          tmptime = (double)(dis - diffdis) / (double)v;
+          time[i] = tmptime;
+          timesort[i] = tmptime;
+     }
+     sort(timesort, timesort + n);
+     for (int i = 0; i < n; i++) {
+          printf("%.6f ", timesort[i]);
+          for (int j = 0; j < n; j++) {
+               if (time[j] == timesort[i]) {
+                    cout << j + 1 << endl;
+                    time[j] = -10e9;
+                    break;
+               }
+          }
+     }
+     return 0;
+}
