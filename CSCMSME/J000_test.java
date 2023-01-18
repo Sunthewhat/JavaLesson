042363d82b4e4e69422d2c81d949c68eb076892b
Sunthewhat <81130744+Sunthewhat@users.noreply.github.com>
2023-01-19 00:27:19 +0700

    sdf

--- CSCMSME/J000_test.java
+++ CSCMSME/J000_test.java
@@ -6,10 +6,11 @@ public class J000_test {
     public static boolean f() {
         return true;
     }
+    // aspidjasd
 
     public static void main(String[] args) {
         java.util.Scanner sc = new java.util.Scanner(System.in);
-        System.out.println(12345%7);
+        System.out.println(12345 % 7);
         sc.close();
     }
 }
